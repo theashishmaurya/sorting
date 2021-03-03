e1c1dd18082c13ce2d8b8bac707ff0f8dedb5c80
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2021-03-03 15:58:24 +0530

    new algo added

--- src/Components/sortingAlgos.js
+++ src/Components/sortingAlgos.js
@@ -1,26 +1,37 @@
-import React from 'react'
+import React from "react";
 
-export const BubbleSort = (array ,setSteps) =>{
-    let funarray = new Array();
-    funarray = [...array] ;
-    let temp = [];
-    setSteps([]);
-    for (let i = 0 ; i < funarray.length-1 ; i++){
-        for(let j = 0 ; j < funarray.length-1 ; j++){
-            if(funarray[j]>funarray[j+1]){
-                [funarray[j],funarray[j+1]] = [funarray[j+1],funarray[j]]
-                temp.push(funarray.slice());
-            }
-        }
+export const BubbleSort = (array, setSteps) => {
+  let funarray = new Array();
+  funarray = [...array];
+  let temp = [];
+  setSteps([]);
+  for (let i = 0; i < funarray.length - 1; i++) {
+    for (let j = 0; j < funarray.length - 1; j++) {
+      if (funarray[j] > funarray[j + 1]) {
+        [funarray[j], funarray[j + 1]] = [funarray[j + 1], funarray[j]];
+        temp.push(funarray.slice());
+      }
     }
-    console.log('qorking')
-    console.log(temp);
-    setSteps(temp);
-  
+  }
+  console.log("qorking");
+  console.log(temp);
+  setSteps(temp);
 
-    return funarray;
-}
- 
-export const InsertionSort = ()=>{
-    console.log('Insertion Sort working');
-}
+  return funarray;
+};
+
+export const InsertionSort = (array, setSteps) => {
+  let funarray = [...array];
+  let temp = [];
+  for (let i = 1; i < funarray.length() - 1; i++) {
+    let j = i - 1;
+    let x = funarray[i];
+    while (j > -1 && funarray[j] < x) {
+      funarray[j + 1] = funarray[j];
+    }
+  }
+};
+export const SelectionSorrt = () => {};
+export const HeapSort = () => {};
+export const MergeSort = () => {};
+export const QuickSort = () => {};
